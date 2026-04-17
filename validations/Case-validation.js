23764475ffff11f09ecc3398ae4957882a319c03
AshanIsuranga <21it0478@itum.mrt.ac.lk>
2026-04-17 16:27:08 +0530

    fix

--- validations/Case-validation.js
+++ validations/Case-validation.js
@@ -3,3 +3,14 @@ const Joi = require("joi");
 exports.getparmasIdSchema = Joi.object({
     caseId: Joi.number().required()
 });
+
+exports.getConnectionSchema = Joi.object({
+    partyId: Joi.number().required(),
+    userId: Joi.number().required()
+});
+
+exports.getAllConnectionDetailsSchema = Joi.object({
+    page: Joi.number().integer().min(1).default(1).optional(),
+    limit: Joi.number().integer().min(1).max(100).default(10).optional(),
+    searchText: Joi.string().allow('').optional(), 
+});
